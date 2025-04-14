2d18f894b46cab761af395d6cf8409323982aa3d
GuvensConsultora <guvensconsultora@gmail.com>
2025-04-14 19:08:43 -0300

    Actualización de versión a 17.1

--- __manifest__.py
+++ __manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'Mercadolibre odoo',
-    'version': '17.0.0.2',
+    'version': '17.1.0.2',
     'description': 'Mercadolibre integration with odoo',
     'summary': 'Mercadolibre integration with odoo',
     'author': 'Vex Soluciones',
